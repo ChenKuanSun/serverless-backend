38c1fdc19f5940084cdf83fb4fe2d37fd096a414
chenkuansun <fgriasa123@gmail.com>
2021-07-30 17:08:18 +0800

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,26 +1,11 @@
-const { AwsCdkConstructLibrary } = require('projen');
-const project = new AwsCdkConstructLibrary({
-  author: 'chenkuansun',
-  authorAddress: 'fgriasa123@gmail.com',
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
   name: 'cdk-serverless-backend',
-  repositoryUrl: 'https://github.com/ChenKuanSun/cdk-serverless-backend.git',
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
-  // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
-  deps: [
-    '@aws-cdk/aws-apigateway',
-    '@aws-cdk/aws-apigatewayv2',
-    '@aws-cdk/aws-apigatewayv2-authorizers',
-    '@aws-cdk/aws-apigatewayv2-integrations',
-    '@aws-cdk/aws-cognito',
-    '@aws-cdk/aws-dynamodb',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-lambda-python',
-    '@aws-cdk/aws-s3',
-    '@aws-cdk/core',
-  ] /* Runtime dependencies of this module. */,
+  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                       /* Build dependencies for this module. */
   // packageName: undefined,            /* The "name" in package.json. */
